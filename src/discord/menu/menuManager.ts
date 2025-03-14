66bceea60bf66736e8fbb729c136d2d92396b89e
Grey <84243322+Shadows-Development@users.noreply.github.com>
2025-03-14 18:36:18 -0500

    feat: Dynamic Custom ID for select menus

--- src/discord/menu/menuManager.ts
+++ src/discord/menu/menuManager.ts
@@ -1,22 +1,31 @@
 import { Client, StringSelectMenuInteraction } from "discord.js";
 import { Menu } from "./menu";
+import { splitSpecialId } from "../util";
 
 export class MenuManager {
-    private client: Client;
-    private static menus: Map<string, Menu> = new Map();
+  private client: Client;
+  private static menus: Map<string, Menu> = new Map();
 
-    constructor(client: Client) {
-        this.client = client;
-    }
+  constructor(client: Client) {
+    this.client = client;
+  }
 
-    registerMenu(menu: Menu) {
-        MenuManager.menus.set(menu.customId, menu);
-    }
+  registerMenu(menu: Menu) {
+    MenuManager.menus.set(menu.customId, menu);
+  }
 
-    async handleInteraction(interaction: StringSelectMenuInteraction) {
-        const menu = MenuManager.menus.get(interaction.customId);
-        if (menu) {
-            await menu.run(interaction, this.client);
-        }
+  async handleInteraction(interaction: StringSelectMenuInteraction) {
+    // const menu = MenuManager.menus.get(interaction.customId);
+    let menu: Menu | undefined;
+    if (interaction.customId.includes(":")) {
+      const parsedId = splitSpecialId(interaction.customId);
+      const newId = `${parsedId.feature}:${parsedId.action}:{id}`;
+      menu = MenuManager.menus.get(newId);
+    } else {
+      menu = MenuManager.menus.get(interaction.customId);
     }
+    if (menu) {
+      await menu.run(interaction, this.client);
+    }
+  }
 }
