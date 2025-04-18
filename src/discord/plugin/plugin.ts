f2d2fb3917be0548f1a15abac4db38e8e2297e41
Grey <mr.hfr1213@gmail.com>
2025-04-18 02:34:21 +0000

    feat(plugin): Added experimental plugin system

--- src/discord/plugin/plugin.ts
+++ src/discord/plugin/plugin.ts
@@ -1,3 +1,13 @@
-export function throwaway1() {
+import { Client } from "discord.js";
 
+export interface Plugin {
+  metadata: metadata;
+  register: (client: Client) => void;
+}
+
+export interface metadata {
+  name: string;
+  version: string;
+  description: string;
+  author?: string;
 }
