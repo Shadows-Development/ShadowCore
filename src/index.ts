a27f5187c1ffcd699665ba4d078be9cab94a96c7
Grey <mr.hfr1213@gmail.com>
2025-07-02 23:22:51 +0000

    fix: Update export for nest modules

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "./discord";
 export * from "./security";
 // Explicitly export ApiKey from './nest' and exclude it from './types'
 export { ApiKey } from "./nest";
+export * from "./nest";
 export * from "./types";
